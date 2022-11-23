f7b8cdce7de76e13b27698c7e3385d7f7ca7cedd
Maribel <maribel.torres.guevara@gmail.com>
2022-11-22 22:33:08 -0500

    Ejercicios S1

---
+++ sesion-1/js/script.js
@@ -0,0 +1,212 @@
+let pokemons = [
+  {
+    id: 1,
+    name: "charmander",
+    type: "fire",
+    base_damage: 10,
+    base_hp: 12,
+    speed: 30,
+  },
+  {
+    id: 2,
+    name: "squirtle",
+    type: "water",
+    base_damage: 9,
+    base_hp: 14,
+    speed: 26,
+  },
+  {
+    id: 3,
+    name: "bulbasaur",
+    type: "leaf",
+    base_damage: 8,
+    base_hp: 16,
+    speed: 26,
+  },
+  {
+    id: 4,
+    name: "pikachu",
+    type: "electric",
+    base_damage: 12,
+    base_hp: 8,
+    speed: 32,
+  },
+  {
+    id: 5,
+    name: "pidgey",
+    type: "air",
+    base_damage: 10,
+    base_hp: 10,
+    speed: 35,
+  },
+  {
+    id: 6,
+    name: "goldeen",
+    type: "water",
+    base_damage: 9,
+    base_hp: 12,
+    speed: 32,
+  },
+  {
+    id: 7,
+    name: "bellsprout",
+    type: "leaf",
+    base_damage: 10,
+    base_hp: 12,
+    speed: 30,
+  },
+  {
+    id: 8,
+    name: "magnemite",
+    type: "electric",
+    base_damage: 9,
+    base_hp: 14,
+    speed: 30,
+  },
+  {
+    id: 9,
+    name: "ponyta",
+    type: "fire",
+    base_damage: 12,
+    base_hp: 18,
+    speed: 36,
+  },
+  {
+    id: 10,
+    name: "evee",
+    type: "normal",
+    base_damage: 10,
+    base_hp: 12,
+    speed: 30,
+  },
+];
+
+//1. Ordernar los pokemons por base_damage de menor a mayor.
+
+function comparar(a, b) {
+  return a.base_damage - b.base_damage;
+}
+
+// pokemons.sort(comparar);
+
+// console.log(pokemons);
+
+//2. Crear una funcion para ordernar los pokemons dependiendo de el argumento
+// que se ingrese en la funcion. Pueden ingresar: type, base_damage, base_hp o speed.
+
+function sortPokemos(argument) {
+  let validarInput = ["type", "base_damage", "speed"];
+
+  if (validarInput.includes(argument)) {
+    let result =
+      argument === "type"
+        ? pokemons.sort((a, b) => a[argument].localeCompare(b[argument]))
+        : pokemons.sort((a, b) => a[argument] - b[argument]);
+
+    console.log(result);
+  } else {
+    console.log("Debes ingresar un argumento válido");
+  }
+}
+
+// sortPokemos("base_damage");
+
+//3. Crear una funcion que filtre el objeto pokemons y devuelva un arreglo con los pokemons filtrados.
+//  La funcion debe aceptar un argumento para filtrar por type de pokemon.
+
+function filtrarPokemos(argument) {
+  let filteredPokemons = pokemons.filter(
+    (pokemon) => pokemon.type === argument
+  );
+  console.log(filteredPokemons);
+}
+
+// filtrarPokemos("electric");
+
+//4. Crear un objeto llamado Pokemon Master que tenga los siguientes atributos:
+//  id: number, name: string, created_date: string, y pokemon: array of objects.
+
+let pokemonMaster = {
+  id: 1,
+  name: "Mar",
+  created_date: "20-07-2021 10:00",
+  pokemon: [],
+};
+
+//5. Crear una funcion que de manera aleatoria agregue un nuevo pokemon al
+//  atributo pokemon de Pokemon Master.
+
+function getRandomInt(length) {
+  return Math.floor(Math.random() * length);
+}
+
+let indice = getRandomInt(pokemons.length);
+// console.log(indice);
+
+function aleatoriaPokemon(pokemon) {
+  pokemonMaster.pokemon.push(pokemon);
+}
+
+// aleatoriaPokemon(pokemons[indice]);
+// console.log(pokemonMaster);
+
+//6. Crear una funcion que agregue de manera aleatoria los atributos min_damage
+//  y max_damage a nuestro arreglo de pokemons teniendo en cuenta lo siguiente:
+// min_damage debe ser un numero entero aleatorio entre 1 y 2 y max_damage debe
+// ser un numero entero aleatorio entre 3 y 5
+
+function agregarAtribu() {
+  for (let i = 0; i < pokemons.length; i++) {
+    let min_damage = Math.floor(Math.random() * (2 - 1 + 1) + 1);
+    let max_damage = Math.floor(Math.random() * (5 - 3 + 1) + 3);
+
+    pokemons[i].min_damage = min_damage;
+    pokemons[i].max_damage = max_damage;
+  }
+
+  // const nuevoarr = pokemons.map((pokemon) => {
+  //   let min_damage = Math.floor(Math.random() * (2 - 1 + 1) + 1);
+  //   let max_damage = Math.floor(Math.random() * (5 - 3 + 1) + 3);
+
+  //   return {
+  //     ...pokemon,
+  //     min_damage: min_damage,
+  //     max_damage: max_damage,
+  //   };
+  // });
+
+  // pokemons = nuevoarr;
+  // console.log(nuevoarr);
+}
+
+agregarAtribu();
+// console.log(pokemons);
+
+//7. Crear una funcion que determine el daño que hara un pokemon elegido de la lista ante una posible pelea, para ello considerar que el daño que hara el pokemon es:
+// daño = base_damage + un valor aleatorio entre el min_damage y el max_damage
+
+function setDamage(indice) {
+  let seletedPokemon = pokemons[indice];
+  let totalDamage =
+    seletedPokemon.base_damage +
+    Math.floor(
+      Math.random() *
+        (seletedPokemon.max_damage - 1 + seletedPokemon.min_damage)
+    );
+
+  // console.log(totalDamage);
+}
+
+// 8. Nuestro Pokemon Master quiere estar preparado para pelear,
+// para ello necesita que lo apoyes a ordenar sus pokemons.
+// El quiere que sus pokemons se ordenen de manera
+// que el que tenga un mayor valor posible de base_damage + max_damage
+// sea el que este primero en la lista y asi sucesivamente.
+
+pokemons.sort((a, b) => {
+  const x = b.base_damage + b.max_damage;
+  const y = a.base_damage + a.max_damage;
+  return x - y;
+});
+
+console.log(pokemons);
