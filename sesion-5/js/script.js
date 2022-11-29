a0122381031c48be5f3ffc21bfd41e0e788682f5
Maribel <maribel.torres.guevara@gmail.com>
2022-11-28 21:22:15 -0500

    Nueva forma de crear selects

--- sesion-5/js/script.js
+++ sesion-5/js/script.js
@@ -1,3 +1,32 @@
+// Otra forma de hacer el formulario
+const categorias = {
+  A: 40,
+  B: 35,
+  C: 30,
+};
+
+const aniosServicio = {
+  "1 - 3": 0.15,
+  "4 - 7": 0.2,
+  "8 - 12": 0.3,
+  "13 - más": 0.35,
+};
+
+function crearSelect(obj, placeholder) {
+  const select = document.createElement("select");
+  const placeholderOpt = document.createElement("option");
+  placeholderOpt.textContent = placeholder;
+  select.appendChild(placeholderOpt);
+
+  for (const property in obj) {
+    const opt = document.createElement("option");
+    opt.textContent = property;
+    opt.value = obj[property];
+    select.appendChild(opt);
+  }
+  return select;
+}
+
 let nombre = prompt("Nombre");
 
 const root = document.getElementById("root");
@@ -8,43 +37,48 @@ const inputHoras = document.createElement("input");
 inputHoras.setAttribute("type", "number");
 inputHoras.setAttribute("placeholder", "Horas trabajadas");
 
-// Selector para catrgorias
-const selectC = document.createElement("select");
-const optionC0 = document.createElement("option");
-optionC0.textContent = "Selecciona la categoria";
+// Crear select a traves de la función crearSelect()
+const selectC = crearSelect(categorias, "Categoria");
+const selectAnios = crearSelect(
+  aniosServicio,
+  "Selecciona los años de servicio"
+);
 
-const option1 = document.createElement("option");
-option1.value = 40;
-option1.textContent = "A";
+// const optionC0 = document.createElement("option");
+// optionC0.textContent = "Selecciona la categoria";
 
-const option2 = document.createElement("option");
-option2.value = 35;
-option2.textContent = "B";
+// const option1 = document.createElement("option");
+// option1.value = 40;
+// option1.textContent = "A";
 
-const option3 = document.createElement("option");
-option3.value = 30;
-option3.textContent = "C";
+// const option2 = document.createElement("option");
+// option2.value = 35;
+// option2.textContent = "B";
+
+// const option3 = document.createElement("option");
+// option3.value = 30;
+// option3.textContent = "C";
 
 // Selector para anios de experiencia
-const selectAnios = document.createElement("select");
-const optionAnios0 = document.createElement("option");
-optionAnios0.textContent = "Selecciona los años de servicio";
+// const selectAnios = document.createElement("select");
+// const optionAnios0 = document.createElement("option");
+// optionAnios0.textContent = "Selecciona los años de servicio";
 
-const optionAnios1 = document.createElement("option");
-optionAnios1.value = 0.15;
-optionAnios1.text = "1 - 3";
+// const optionAnios1 = document.createElement("option");
+// optionAnios1.value = 0.15;
+// optionAnios1.text = "1 - 3";
 
-const optionAnios2 = document.createElement("option");
-optionAnios2.value = 0.2;
-optionAnios2.text = "4 - 7";
+// const optionAnios2 = document.createElement("option");
+// optionAnios2.value = 0.2;
+// optionAnios2.text = "4 - 7";
 
-const optionAnios3 = document.createElement("option");
-optionAnios3.value = 0.3;
-optionAnios3.text = "8 - 12";
+// const optionAnios3 = document.createElement("option");
+// optionAnios3.value = 0.3;
+// optionAnios3.text = "8 - 12";
 
-const optionAnios4 = document.createElement("option");
-optionAnios4.value = 0.35;
-optionAnios4.text = "13 - más";
+// const optionAnios4 = document.createElement("option");
+// optionAnios4.value = 0.35;
+// optionAnios4.text = "13 - más";
 
 const botonSubmit = document.createElement("button");
 botonSubmit.setAttribute("type", "submit");
@@ -59,15 +93,15 @@ let pSueldo = document.createElement("p");
 let pBonificacion = document.createElement("p");
 let pSueldoNeto = document.createElement("p");
 
-selectC.appendChild(optionC0);
-selectC.appendChild(option1);
-selectC.appendChild(option2);
-selectC.appendChild(option3);
-selectAnios.appendChild(optionAnios0);
-selectAnios.appendChild(optionAnios1);
-selectAnios.appendChild(optionAnios2);
-selectAnios.appendChild(optionAnios3);
-selectAnios.appendChild(optionAnios4);
+// selectC.appendChild(optionC0);
+// selectC.appendChild(option1);
+// selectC.appendChild(option2);
+// selectC.appendChild(option3);
+// selectAnios.appendChild(optionAnios0);
+// selectAnios.appendChild(optionAnios1);
+// selectAnios.appendChild(optionAnios2);
+// selectAnios.appendChild(optionAnios3);
+// selectAnios.appendChild(optionAnios4);
 
 root.appendChild(form);
 form.appendChild(inputHoras);
